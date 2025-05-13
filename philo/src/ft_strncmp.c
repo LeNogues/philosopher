f4db2de32442c10a207da19bf02d5a771f922e1c
LeNogues <sebastienlenogues@gmail.com>
2025-05-13 17:15:48 +0200

    final

--- philo/src/ft_strncmp.c
+++ philo/src/ft_strncmp.c
@@ -1,30 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stop_simulation.c                                  :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seb <seb@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/07 13:48:50 by seb               #+#    #+#             */
-/*   Updated: 2025/05/13 17:14:27 by seb              ###   ########.fr       */
+/*   Created: 2024/11/08 09:47:10 by sle-nogu          #+#    #+#             */
+/*   Updated: 2025/05/13 17:12:28 by seb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
-int	stop_simulation(t_arg *arg, t_philo *philo)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	pthread_mutex_lock(&arg->stop_lock);
-	if (arg->should_stop == 1)
+	size_t	i;
+
+	i = 0;
+	while (i < n)
 	{
-		pthread_mutex_unlock(&arg->stop_lock);
-		return (0);
+		if ((unsigned char)s1[i] != (unsigned char)s2[i] || !s2[i] || !s1[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
 	}
-	arg->should_stop = 1;
-	pthread_mutex_unlock(&arg->stop_lock);
-	if (philo != NULL)
-	{
-		print_status(arg, philo->id, "died");
-	}
-	return (1);
+	return (0);
 }
--- philo/src/print_status.c
+++ philo/src/print_status.c
@@ -6,7 +6,7 @@
 /*   By: seb <seb@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 14:01:59 by seb               #+#    #+#             */
-/*   Updated: 2025/05/11 13:43:02 by seb              ###   ########.fr       */
+/*   Updated: 2025/05/13 17:13:54 by seb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	print_status(t_arg *arg, int philo_id, char *status)
 	unsigned long	timestamp_ms;
 
 	pthread_mutex_lock(&arg->write_lock);
-	if (check_simulation_continue(arg) || strcmp(status, "died") == 0)
+	if (check_simulation_continue(arg) || strncmp(status, "died", 4) == 0)
 	{
 		timestamp_ms = get_current_time_ms() - arg->start_time;
 		printf("%lu %d %s\n", timestamp_ms, philo_id + 1, status);
